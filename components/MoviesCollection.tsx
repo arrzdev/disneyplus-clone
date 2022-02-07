8e77b2370cadf3f2a9f2bd1f4c1d541d40e86d8a
arrz.dev <55147716+arrzdev@users.noreply.github.com>
2022-02-07 10:45:13 +0000

    Added styles on collection scrollbars :rainbow:

--- components/MoviesCollection.tsx
+++ components/MoviesCollection.tsx
@@ -18,7 +18,7 @@ const MoviesCollection:React.FC<IProps> = (props) => {
     return (
         <div className="relative flex flex-col md: space-y-2 my-10 px-8 max-w-[1400px] mx-auto">
             <h2 className="font-semibold select-none">{props.title}</h2>
-            <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 -m-2">
+            <div className="flex space-x-6 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 -m-2 scrollbar">
                 {
                 props.results.map((result) => (
                 <MovieThumbnail key={result.title} poster_path={result.poster_path}/>
